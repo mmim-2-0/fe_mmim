25f300d498119796f6b729c12610a37889d7ef3c
Olivia Whitehead <oliviawhitehead3@gmail.com>
2022-08-21 18:31:26 -0600

    Add a very basic user form

--- src/Components/UserMidForm/UserMidForm.js
+++ src/Components/UserMidForm/UserMidForm.js
@@ -2,7 +2,24 @@ import React from 'react';
 
 const UserMidForm = () => {
     return (
-        <p>User Mid Form!</p>
+        <section>
+            <button>Set your default address</button>
+            <form>
+            <p>User Mid Form!</p>
+            <input type='text' placeholder='Address 1'></input>
+            <p>OR</p>
+            <input type='text' placeholder='Other User email'></input>
+            <input type='text' placeholder='Address 2'></input>
+            <select>
+                <option>Cafe</option>
+                <option>Restaurant</option>
+                <option>Bar</option>
+                <option>Park</option>
+                <option>Library</option>
+            </select>
+            <button>Find a Midpoint</button>
+        </form>
+    </section>
     )
 };
 
