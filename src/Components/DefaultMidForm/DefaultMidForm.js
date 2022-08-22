5efbed684eb75364f1aa3312da0246b8d3783682
Olivia Whitehead <oliviawhitehead3@gmail.com>
2022-08-21 18:31:18 -0600

    Add a very basic default form

--- src/Components/DefaultMidForm/DefaultMidForm.js
+++ src/Components/DefaultMidForm/DefaultMidForm.js
@@ -2,7 +2,19 @@ import React from 'react';
 
 const DefaultMidForm = () => {
     return (
-        <p>Default Mid Form!</p>
+        <form>
+            <p>Default Mid Form!</p>
+            <input type='text' placeholder='Address 1'></input>
+            <input type='text' placeholder='Address 2'></input>
+            <select>
+                <option>Cafe</option>
+                <option>Restaurant</option>
+                <option>Bar</option>
+                <option>Park</option>
+                <option>Library</option>
+            </select>
+            <button>Find a Midpoint</button>
+        </form>
     )
 };
 
