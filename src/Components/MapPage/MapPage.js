d90ee3eb0a13df6496b27155a2b37a2f2fe9a0e8
Olivia Whitehead <oliviawhitehead3@gmail.com>
2022-08-21 18:18:36 -0600

    Add basic MapPage component

---
+++ src/Components/MapPage/MapPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Map from '../Map/Map'
+import AllResults from '../AllResults/AllResults'
+
+const MapPage = () => {
+    return (
+        <section>
+            <p>Map Page!</p>
+            <Map />
+            <AllResults />
+        </section>
+    )
+};
+
+export default MapPage;
