979537b4fd7408a2d3d77c836bd1a3060231e49d
Nikhila Balmoori <nbalmoori@gmail.com>
2022-08-29 13:31:06 -0600

    Create fetch call to test backend

---
+++ src/apiCalls.js
@@ -0,0 +1,15 @@
+const getFetch = () => {
+    return fetch(`https://serene-thicket-09827.herokuapp.com/api/v1/search?add1=denver&add2=austin&keyword=cafe`)
+    .then(response => {
+      if (!response.ok) {
+        throw Error(response.text)
+      } else {
+        return response.json()
+      }
+    })
+    .catch(err => {
+        console.log(err)
+    })
+}
+
+export default getFetch;
