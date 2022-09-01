3f561340fe4f15f47e5d3286f2d30b00eefa1a99
Olivia Whitehead <oliviawhitehead3@gmail.com>
2022-09-01 12:22:42 -0600

    Add form values to state

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import UserMidForm from './Components/UserMidForm/UserMidForm';
 import MapPage from './Components/MapPage/MapPage';
 import Login from './Components/login';
 import Logout from './Components/logout';
-import getFetch from './apiCalls.js';
+// import getFetch from './apiCalls.js';
 import { BrowserRouter as Router,  Routes,  Route } from "react-router-dom";
 import { useEffect, useState } from 'react';
 import { gapi } from 'gapi-script';
@@ -18,6 +18,12 @@ function App() {
   const [userEmail, setUserEmail] = useState(null)
   const [userName, setUserName] = useState(null)
   // if userEmail is null, show not logged in page 
+  const [searchCategory, setSearchCategory] = useState('cafe')
+  const [addressOne, setAddressOne] = useState(null)
+  const [addressTwo, setAddressTwo] = useState(null)
+  const [searchResponses, setSearchResponses] = useState([])
+
+
 
   useEffect(() => {
     function start() {
@@ -25,12 +31,13 @@ function App() {
         clientId: clientId,
         scope: ""
       })
+      // getFetch('denver', 'austin', 'cafe')
     }
 
     gapi.load('client:auth2', start)
   })
 
-  console.log(getFetch())
+  // console.log(getFetch())
   return (
     <Router>
       <div className="App">
@@ -47,7 +54,19 @@ function App() {
           setUserName={setUserName}
         />
         <Routes>
-          <Route path='/' element={<Homepage userEmail={userEmail} userName={userName}/>}/>
+          <Route path='/' element={<Homepage 
+            userEmail={userEmail} 
+            userName={userName}
+            searchCategory={searchCategory}
+            setSearchCategory={setSearchCategory}
+            addressOne={addressOne}
+            setAddressOne={setAddressOne}
+            addressTwo={addressTwo}
+            setAddressTwo={setAddressTwo}
+            searchResponses={searchResponses}
+            setSearchResponses={setSearchResponses}
+            />}
+          />
           <Route path='/results' element={<ResultsPage/>}/>
         </Routes>
       </div>
