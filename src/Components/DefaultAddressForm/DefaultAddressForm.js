f2a24cd51e4de5512b65778e2434a5b58febfa13
Olivia Whitehead <oliviawhitehead3@gmail.com>
2022-09-10 10:36:53 -0600

    Add basic logic to default address form

--- src/Components/DefaultAddressForm/DefaultAddressForm.js
+++ src/Components/DefaultAddressForm/DefaultAddressForm.js
@@ -1,8 +1,20 @@
 import React from 'react';
 
-const DefaultAddressForm = () => {
+const DefaultAddressForm = ({ setUserDefaultAddress }) => {
+    
+    const defaultAddressHandler = (e) => {
+        setUserDefaultAddress(e.target.value)
+    }
+
+    const submitDefaultAddress = () => {
+        // Fetch PUT to update the user's default address in the backend!
+    }
+
     return (
-        <p>Default Address Form!</p>
+        <form>
+            <input type="text" placeholder="Set your default address here." onChange={defaultAddressHandler}></input>
+            <button onClick={submitDefaultAddress}>Save your address</button>
+        </form>
     )
 };
 
