ebf2638a149391d1f546298497d67c71c12ce864
Olivia Whitehead <oliviawhitehead3@gmail.com>
2022-09-25 21:05:36 -0600

    Add classes to style results

--- src/Components/Result/Result.js
+++ src/Components/Result/Result.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useState } from 'react';
-
+import './Result.css';
 
 const Result = ({ info, checkedMeetingLocations, setCheckedMeetingLocations, id, searchResponses, addressTwoEmail }) => {
 
@@ -19,16 +19,16 @@ const Result = ({ info, checkedMeetingLocations, setCheckedMeetingLocations, id,
     }
 
     return (
-      <div>
-        <img src={info.photos[0]}/>
+      <div className="individual-result">
         <h2>{info.name}</h2>
-        <p>{info.review_count} ratings</p>
-        <p>{info.categories.join(', ')}</p>
-        <p>{info.price}</p>
-        <p>{info.address}</p>
-        {!info.is_open_now ? <p>Currently Closed</p> : <p>Open Now</p>}
+        <p className="result-info"> {info.review_count} ratings</p>
+        <p className="result-info">{info.categories.join(', ')}</p>
+        <p className="result-info">{info.price}</p>
+        <p className="result-info">{info.address}</p>
+        {!info.is_open_now ? <p className="result-info">Currently Closed</p> : <p>Open Now</p>}
+        {info.photos[0] && <img className="result-image" src={info.photos[0]}/>}
         {addressTwoEmail && 
-          <div>
+          <div className="invite-info">
             <p>Meet here</p>
             <input type="checkbox" id={id} checked={checked} onChange={handleCheckBox}/>
           </div>}
