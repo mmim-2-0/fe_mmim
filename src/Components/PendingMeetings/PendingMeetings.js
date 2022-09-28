38f1ac6e9f15f212f98caa213115973684de1134
Olivia Whitehead <oliviawhitehead3@gmail.com>
2022-09-28 11:53:36 -0600

    Change dashboard title wording

--- src/Components/PendingMeetings/PendingMeetings.js
+++ src/Components/PendingMeetings/PendingMeetings.js
@@ -19,7 +19,7 @@ const PendingMeetings = ({ userMeetings, userId, token }) => {
 
     return (
       <div className="all-pending-and-title">
-        <h2 className="pending-title">Your Pending Meetings</h2>
+        <h2 className="pending-title">your pending meetings:</h2>
         <div className="all-pending">
           {displayPendingMeetings}
         </div>
