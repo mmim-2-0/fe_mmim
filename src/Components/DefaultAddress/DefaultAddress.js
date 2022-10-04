26504fd030996a6e827a25178ef038e4abcc2e4d
Nikhila Balmoori <nbalmoori@gmail.com>
2022-10-04 00:33:55 -0600

    Update styling for my info section on dashboard

--- src/Components/DefaultAddress/DefaultAddress.js
+++ src/Components/DefaultAddress/DefaultAddress.js
@@ -24,9 +24,14 @@ const DefaultAddress = ({ token, userName, userEmail, userDefaultAddress, setUse
 
   return (
     <div>
-      <h3 className="change-default-title">change your default address:</h3>
-      <input className="default-input" type="text" placeholder="new default address" value={localDefault} onChange={handleLocalDefault}></input>
-      <button className="update-button" onClick={defaultAddressHandler}>update</button>
+      <h2 className="myinfo-title">My Info</h2>
+      <div className="myinfo-container">
+        <p className="username">Username: {userName}</p>
+        <p className="default-address"><strong>Default Address:</strong> {userDefaultAddress}</p>
+        <h3 className="change-default-title">change your default address:</h3>
+        <input className="default-input" type="text" placeholder="new default address" value={localDefault} onChange={handleLocalDefault}></input>
+        <button className="update-button" onClick={defaultAddressHandler}>update</button>
+      </div>
     </div>
   )
 }
