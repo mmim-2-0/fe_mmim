3edae2702a5b6934816f1477af40c02202552f90
Olivia Whitehead <oliviawhitehead3@gmail.com>
2022-10-05 11:17:54 -0600

    Filter pending meetings to not show duplicates

--- src/Components/PendingMeetings/PendingMeetings.js
+++ src/Components/PendingMeetings/PendingMeetings.js
@@ -1,11 +1,32 @@
 import React from 'react';
 import PendingMeeting from '../PendingMeeting/PendingMeeting';
 import './PendingMeetings.css';
+import { useState, useEffect } from 'react';
 
 const PendingMeetings = ({ userMeetings, userId, token, setUserMeetings }) => {
 
-  const pendingMeetings = userMeetings.filter(meeting => meeting.attributes.status === "pending");
-  
+  const [pendingMeetings, setPendingMeetings] = useState([]);
+
+  useEffect(() => {
+    let allMeetings = []
+    userMeetings.forEach((m) => {
+      if (m.attributes.status === 'pending' && !allMeetings.includes(m)) {
+        allMeetings.push(m)
+      }
+    })
+    let filteredMeetings = [];
+    allMeetings.forEach(m => {
+      let allIds = filteredMeetings.reduce((acc, i) => {
+        acc.push(i.id)
+        return acc;
+      }, [])
+      if (!allIds.includes(m.id)) {
+        filteredMeetings.push(m);
+      }
+    })
+    setPendingMeetings(filteredMeetings)
+  }, [])
+
   const displayPendingMeetings = pendingMeetings.map((meeting, index) => {
     return <PendingMeeting 
       userId={userId}
