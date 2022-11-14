c99f6ab4d719d5a5a61998fc283cae263f524304
Kim G <xiaole.guo@yahoo.com>
2022-11-13 19:16:03 -0700

    delete duplicate line

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -156,7 +156,6 @@ const patchMeeting = (status, userId, meetingId, token, locationId) => {
   })
 }
 
-export { getLocations, getUser, getUserMeetings, logoutUser, updateDefaultAddress, sendMeetingOptions, getGuestUser, patchMeeting };
 
 const getCurrentLocation = (location) => {
   return fetch(`https://www.mapquestapi.com/geocoding/v1/reverse?location=${location}&key=${process.env.REACT_APP_MAPQUEST_API_KEY}`, {
