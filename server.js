b284ca132c3295320d4b9062371c0297aa9de577
Kim G <xiaole.guo@yahoo.com>
2023-01-22 09:33:36 -0700

    specify static pathaway

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ app.use(express.static(__dirname + '/'));
 app.listen(process.env.PORT || 8080);
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('build'));
+    app.use(express.static(path.join(__dirname, '../build')));
     app.get('*', (req, res) => {
       const index = path.join(__dirname, 'public', 'index.html');
       res.sendFile(index);
